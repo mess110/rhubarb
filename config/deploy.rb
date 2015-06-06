4ebe2574c1936116cfa90db6437519ab92f0fa17
Cristian Mircea Messel <mess110@gmail.com>
2015-06-06 19:33:19 +0300

    update ruby version

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,14 @@
 # config valid only for current version of Capistrano
 lock '3.4.0'
 
-set :application, 'rhubarb-cron-client'
+set :application, 'rhubarb'
 set :repo_url, 'https://github.com/mess110/rhubarb'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, '/home/kiki/rhubarb'
+set :deploy_to, '/home/kiki/rhubarb.northpole.ro'
 
 # Default value for :scm is :git
 # set :scm, :git
