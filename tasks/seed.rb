e4a4094ef40ac35c73fd783f084d93b2e83eba7d
Cristian Mircea Messel <mess110@gmail.com>
2015-06-11 21:10:12 +0300

    update seed file

--- tasks/seed.rb
+++ tasks/seed.rb
@@ -4,4 +4,5 @@ Rhubarb.all.each do |h|
   RhubarbItem.create h
 end
 
-puts "RhubarbItem count: #{RhubarbItem.count}"
+puts "There are #{RhubarbItem.count} items"
+puts "and #{RhubarbUser.count} users in teh db." # intentional typo
