27871a88c88ed37d5f88172cc5f1bfb01cd17c08
Cristian Mircea Messel <mess110@gmail.com>
2015-06-12 00:20:53 +0300

    include scripts

---
+++ scripts/cron.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+source /home/kiki/.rvm/environments/ruby-2.1.0@rhubarb
+cd /home/kiki/rhubarb.northpole.ro/current/
+RACK_ENV=production ki tasks notify 2>&1 | tee log/cron.sh.log
